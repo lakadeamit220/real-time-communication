030691b9f7ca34e4f9382212b3c752ba952ed8aa
Amit Lakade <99075733+lakadeamit220@users.noreply.github.com>
2025-06-18 12:16:18 +0530

    Implemented longPolling.

---
+++ backend/longPolling.js
@@ -0,0 +1,22 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/poll') {
+    setTimeout(() => {
+      res.writeHead(200, {
+        "content-type": "application/json",
+      })
+      res.end(JSON.stringify({
+        message: `Hello at ${new Date().toISOString()}`
+      }))
+    }, 3000)
+  } else {
+    res.writeHead(200);
+    res.end("server is up and running!!")
+  }
+})
+
+server.listen(3000,()=>{
+  console.log("Server is running on port 3000");
+  
+})
